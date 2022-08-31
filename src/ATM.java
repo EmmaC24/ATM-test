7ed885619e575ad056f108b1d9e1d4be29e863de
Emma <emmacmiller24@icloud.com>
2022-08-31 13:18:13 -0700

    prevent going into debt and no negative money

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,7 @@
+
+
 import java.util.HashMap;
+import java.util.*;
 
 public class ATM {
 	private int id;
@@ -38,19 +41,25 @@ public class ATM {
 	
 	public double checkBalance(int accountID)
 	{
-		if (!accounts.containsKey (accountID))
+		double currentBalance = 0.0;
+		if (accounts.containsKey (accountID))
 		{
-			return 0.0;
+			currentBalance = accounts.get(accountID);
+		}
+		
+		if (accounts.containsKey (accountID))
+		{
+			currentBalance = accounts.get(accountID);
+			return currentBalance;
 		}
 		else
 		{
-			return accounts.get (accountID);
+			return 0.0;
 		}
 	}
 	
 	public boolean depositMoney (int accountID, double money)
 	{
-	
 		if (accounts.containsKey (accountID))
 		{
 			accounts.replace (accountID, accounts.get(accountID) + money);
@@ -77,15 +86,53 @@ public class ATM {
 	
 	public static void main (String [] args)
 	{
-		ATM chaseBank = new ATM();
+		//ATM chaseBank = new ATM();
 		
-		chaseBank.openAccount(12, 5.0);
-		System.out.println (chaseBank.checkBalance (12));
-		chaseBank.withdrawMoney(12, 3.0);
-		System.out.println (chaseBank.checkBalance (12));
+		//chaseBank.openAccount(12, 5.0);
+		//System.out.println (chaseBank.checkBalance (12));
+		//chaseBank.withdrawMoney(12, 3.0);
+		//System.out.println (chaseBank.checkBalance (12));
 		
-		chaseBank.depositMoney(12, 100);
-		System.out.println (chaseBank.checkBalance (12));
+		//chaseBank.depositMoney(12, 100);
+		//System.out.println (chaseBank.checkBalance (12));
 		
+		
+		ATM chase = new ATM();
+		
+		chase.openAccount(00001);
+		chase.openAccount(00002, 2500.0);
+		
+		chase.depositMoney(00001, 433.45);
+		chase.depositMoney(00001, 434.77);
+		chase.depositMoney(00001, 424.91);
+		chase.depositMoney(00001, 474.10);
+		chase.depositMoney(00002, 154.30);
+		System.out.println(chase.checkBalance(00001)); // Should be 1767.23
+		System.out.println(chase.checkBalance(00002)); // Should be 2654.30
+		chase.withdrawMoney(00001, 100.00);
+		chase.withdrawMoney(00001, 1000.00);
+		System.out.println(chase.checkBalance(00001)); // Should be 667.23
+		// Test Invalid Deposits
+		chase.depositMoney(00003, -433.45);
+		chase.depositMoney(00004, 32434.77);
+		chase.depositMoney(01337, 128537424.91);
+		chase.depositMoney(21504, 2343474.10);
+		System.out.println(chase.checkBalance(00003)); // Should be 0.0
+		System.out.println(chase.checkBalance(00004)); // Should be 0.0
+		System.out.println(chase.checkBalance(01337)); // Should be 0.0
+		System.out.println(chase.checkBalance(21504)); // Should be 0.0
+		// Test Invalid Withdrawals
+		chase.withdrawMoney(00001, -433.45);
+		chase.withdrawMoney(00001, 32434.77);
+		chase.withdrawMoney(01337, 128537424.91);
+		chase.withdrawMoney(21504, -2343474.10);
+		System.out.println(chase.checkBalance(00001)); // Should be 667.23
+		System.out.println(chase.checkBalance(00001)); // Should be 667.23
+		System.out.println(chase.checkBalance(01337)); // Should be 0.0
+		System.out.println(chase.checkBalance(21504)); // Should be 0.0
+		// Test other issues
+		chase.withdrawMoney(00002, 2020.2);
+		System.out.println(chase.checkBalance(00002)); // Should be 634.1 and 
+		//not a fraction more!
 	}
 }
